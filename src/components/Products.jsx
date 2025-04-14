090ac3974b2db3203b2ee99273cd80b4a541af4a
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-04-14 11:12:05 -0600

    Add show grafica button

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,13 +2,18 @@ import React, { useEffect, useState } from 'react'
 import {Grafica} from './Grafica';
 
 const Products = ({ obtenerDatos, listProducts }) => {
+  const [mostrarGrafica, setMostrarGrafica] = useState(false);
 
   useEffect(() => {
     obtenerDatos();
   }, []);
 
   return (
+    <>
+      {mostrarGrafica ? (<Grafica listProducts={listProducts} obtenerDatos={obtenerDatos}/>
+    ): (
       <>
+      <button onClick={() => setMostrarGrafica(true)}>Ver gráfico</button>
       <table>
         <thead>
           <tr>
