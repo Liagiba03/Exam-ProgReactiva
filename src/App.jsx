639d528640f94de898fc13e8733018c7a1729865
Suseth Sandoval <143125742+Liagiba03@users.noreply.github.com>
2025-04-14 11:07:07 -0600

    Add filter

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,13 @@ function App() {
     }
   }
 
- 
+  const filtrarProductos = () => {
+    const filtro = listProducts.filter((producto) => 
+      producto.title.toLowerCase().includes(busqueda.toLowerCase()) ||
+      producto.category.toLowerCase().includes(busqueda.toLowerCase())
+    );
+    setFiltredList(filtro);
+  }
 
     useEffect(()=>{
     obtenerDatos();
